4b210ec5f751b3403fc08807f685b31585276cba
Nastya Kladova <nstsologub@gmail.com>
2023-11-02 22:57:18 +0100

    + add palette to the theme

--- src/styles/theme/commonTheme.ts
+++ src/styles/theme/commonTheme.ts
@@ -9,10 +9,38 @@ const workSans = Work_Sans({
 });
 
 const theme = createTheme({
+  palette: {
+    mode: 'light',
+    primary: {
+      main: '#0a1047',
+    },
+    secondary: {
+      main: '#6e314a',
+    },
+    error: {
+      main: '#fe645e',
+    },
+    text: {
+      primary: '#000',
+      secondary: '#5c5c5c',
+    },
+    background: {
+      default: '#fff',
+    },
+    grey: {
+      A100: '#98a2b3',
+      A200: '#797979',
+      A400: '#494949',
+      A700: '#0d0d0d',
+    },
+  },
+
   typography: {
     fontFamily: workSans.style.fontFamily,
     fontSize: 16,
     fontWeightRegular: 500,
+
+    h1: {},
   },
 });
 
