795f59d89ac64130d792a0edb790da34c2c353cf
Nastya Kladova <nstsologub@gmail.com>
2023-11-03 20:57:02 +0100

    + overide default components styles

--- src/styles/theme/commonTheme.ts
+++ src/styles/theme/commonTheme.ts
@@ -1,22 +1,9 @@
-import {createTheme} from '@mui/material';
-import {Work_Sans} from 'next/font/google';
-
-const workSans = Work_Sans({
-  weight: ['300', '400', '500', '600'],
-  style: 'normal',
-  display: 'swap',
-  subsets: ['latin'],
-});
+import {createTheme, responsiveFontSizes} from '@mui/material';
+import {inter, workSans} from './fonts';
 
 let theme = createTheme({
   palette: {
     mode: 'light',
-    primary: {
-      main: '#0a1047',
-    },
-    secondary: {
-      main: '#6e314a',
-    },
     error: {
       main: '#fe645e',
     },
@@ -37,13 +24,68 @@ let theme = createTheme({
 
   typography: {
     fontFamily: workSans.style.fontFamily,
+    fontWeightBold: 600,
     fontSize: 16,
-    fontWeightRegular: 500,
+    allVariants: {
+      fontWeight: 500,
+    },
 
     h1: {
       fontSize: 45,
     },
   },
+  components: {
+    MuiOutlinedInput: {
+      styleOverrides: {
+        root: ({theme}) => ({
+          border: 1,
+          borderColor: theme.palette.grey[400],
+          borderRadius: theme.spacing(),
+        }),
+        input: ({theme}) => ({
+          fontSize: 15,
+          fontWeight: 300,
+          color: theme.palette.grey[400],
+        }),
+      },
+    },
+    MuiInputLabel: {
+      styleOverrides: {
+        root: ({theme}) => ({
+          fontSize: 15,
+          color: theme.palette.grey[400],
+        }),
+      },
+    },
+    MuiFormHelperText: {
+      styleOverrides: {
+        root: ({theme}) => ({
+          fontFamily: inter.style.fontFamily,
+          fontSize: 12,
+          fontWeight: 300,
+          color: theme.palette.error.main,
+        }),
+      },
+    },
+    MuiButton: {
+      styleOverrides: {
+        root: ({theme}) => ({
+          textTransform: 'none',
+          backgroundColor: theme.palette.common.white,
+        }),
+        contained: ({theme}) => ({
+          backgroundColor: theme.palette.error.main,
+        }),
+        outlined: ({theme}) => ({
+          color: theme.palette.error.main,
+          borderColor: theme.palette.error.main,
+          borderRadius: theme.spacing(),
+        }),
+      },
+    },
+  },
 });
+
+theme = responsiveFontSizes(theme);
 
 export default theme;
