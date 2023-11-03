fec08570a6aa780391182a5c3be21762d4843e84
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-11-03 06:09:26 +0100

    + create button menu

---
+++ src/components/Product/ButtonMenu.tsx
@@ -0,0 +1,28 @@
+import {Menu, MenuItem, MenuList, MenuProps} from '@mui/material';
+
+const styles = {
+  menuList: {
+    width: '7rem',
+    padding: '0.25rem 0.75rem',
+  },
+  menuItem: {
+    padding: '0.5rem 0',
+  },
+};
+
+const ButtonMenu = (props: MenuProps) => {
+  return (
+    <Menu {...props}>
+      <MenuList sx={styles.menuList}>
+        <MenuItem sx={styles.menuItem} divider>
+          View
+        </MenuItem>
+        <MenuItem sx={styles.menuItem} divider>
+          Edit
+        </MenuItem>
+        <MenuItem sx={styles.menuItem}>Delete</MenuItem>
+      </MenuList>
+    </Menu>
+  );
+};
+export default ButtonMenu;
