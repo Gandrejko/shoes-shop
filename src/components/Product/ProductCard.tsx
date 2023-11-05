c4f8775f96d6082fdf437a4d915bc094e3aa4a67
Antonina Vechorko <antonina.vechorko@gmail.com>
2023-11-05 21:53:28 +0300

    + create header: * fix icons

--- src/components/Product/ProductCard.tsx
+++ src/components/Product/ProductCard.tsx
@@ -1,5 +1,6 @@
 import {useState} from 'react';
 import Image from 'next/image';
+// @ts-ignore
 import {MoreHoriz} from '@mui/icons-material';
 import {
   Box,
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,6 +2,8 @@ import {FilterSidebar} from '@/components/FilterSidebar/FilterSidebar';
 import React from 'react';
 import {CustomButton} from '../components/Button/Button';
 import {useSession} from 'next-auth/react';
+import {Header} from '@/components/ Header/Header';
+
 
 export default function Home() {
   // const {data: session} = useSession();
@@ -9,6 +11,7 @@ export default function Home() {
   return (
     <>
       {/* <CustomButton width="90%" isTransparent={false}>submit</CustomButton> */}
+      <Header />
     </>
   );
 }
