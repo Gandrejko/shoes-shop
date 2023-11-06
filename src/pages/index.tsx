869a3983feb39bb0f2a6503654e8762183f59fc7
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-11-06 13:15:29 +0100

    clean up index page

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,3 @@
-import React from 'react';
-import {CustomButton} from '../components/Button/Button';
-
 export default function Home() {
-  return (
-    <>
-      {/* <CustomButton width="90%" isTransparent={false}>submit</CustomButton> */}
-    </>
-  );
+  return <></>;
 }
