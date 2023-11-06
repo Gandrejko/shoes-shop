169f1d8ca92e02fe664fe4844cf2f5d4263a6b5c
Nastya Kladova <nstsologub@gmail.com>
2023-11-07 00:43:28 +0100

    * change checkbox styles

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,7 +1,6 @@
 import React from 'react';
 import {CustomButton} from '../components/Button/Button';
 import {useSession} from 'next-auth/react';
-import {Checkbox, Divider} from '@mui/material';
 
 export default function Home() {
   // const {data: session} = useSession();
@@ -9,8 +8,6 @@ export default function Home() {
   return (
     <>
       {/* <CustomButton width="90%" isTransparent={false}>submit</CustomButton> */}
-      <Checkbox />
-      <Divider />
     </>
   );
 }
