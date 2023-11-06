a5bcd62da98c84f99a33fd887215f1f43e1aa232
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-11-06 02:23:10 +0100

    + create my products page & add sidebar

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,5 +1,23 @@
+import {CssBaseline} from '@mui/material';
+import {NextPage} from 'next';
 import type {AppProps} from 'next/app';
+import {ReactElement, ReactNode} from 'react';
 
-export default function App({Component, pageProps}: AppProps) {
-  return <Component {...pageProps} />;
+export type NextPageWithLayout<P = {}, IP = P> = NextPage<P, IP> & {
+  getLayout?: (page: ReactElement) => ReactNode;
+};
+
+type AppPropsWithLayout = AppProps & {
+  Component: NextPageWithLayout;
+};
+
+export default function App({Component, pageProps}: AppPropsWithLayout) {
+  const getLayout = Component.getLayout ?? (page => page);
+
+  return getLayout(
+    <>
+      <CssBaseline />
+      <Component {...pageProps} />
+    </>,
+  );
 }
