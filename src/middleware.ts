8aec91d365550ccf907bb7c76f289af5727eac9d
Yura <jormungand1616@gmail.com>
2023-11-08 01:02:42 +0300

    delete reduntant code

--- src/middleware.ts
+++ src/middleware.ts
@@ -1,20 +1,25 @@
+import {NextResponse} from 'next/server';
+import type {NextRequest} from 'next/server';
+
+// This function can be marked `async` if using `await` inside
 import {getToken} from 'next-auth/jwt';
-import {NextRequest, NextResponse} from 'next/server';
 
 export async function middleware(req: NextRequest) {
   const pathname = req.nextUrl.pathname;
-  // List of protected routes
-  const protectedPaths = ['/my-products'];
-  const isPathProtected = protectedPaths?.some(path => pathname == path);
   const res = NextResponse.next();
-  if (isPathProtected) {
-    //it is session info
-    const token = await getToken({req});
-    if (!token) {
-      const url = new URL(`/`, req.url);
-      //url.searchParams.set("callbackUrl", pathname);
-      return NextResponse.redirect(url);
-    }
+
+  //it is session info
+  const token = await getToken({req});
+  if (!token) {
+    const url = new URL(`/`, req.url);
+    //url.searchParams.set("callbackUrl", pathname);
+    return NextResponse.redirect(url);
   }
+
   return res;
 }
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/my-products',
+};
