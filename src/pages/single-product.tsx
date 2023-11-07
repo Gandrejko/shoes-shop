5e87ac68b266cdffc2f4bcbf3bebe9e58c593995
ste1nerr <oachkevych@gmail.com>
2023-11-07 23:52:17 +0200

    + finish write structure

--- src/pages/single-product.tsx
+++ src/pages/single-product.tsx
@@ -1,128 +1,163 @@
-import { Box, Container, Typography, SxProps, Card, CardMedia } from '@mui/material';
+import React from 'react';
+import { Box, Container, Typography, SxProps, Card, CardMedia, Grid } from '@mui/material';
 import products from '@/temp/data';
-import { Button } from '@/components/Button/Button';
 import ImageSlider from '@/components/ImageSlider/ImageSlider';
-import Header from '@/components/Header'
+import Header from '@/components/Header';
+import { Button } from '@/components/Button/Button';
+const sizes = ['EU-36', 'EU-37', 'EU-38', 'EU-39', 'EU-40', 'EU-41', 'EU-42', 'EU-43', 'EU-44', 'EU-45'];
+
+
 
 const styles: Record<string, SxProps> = {
-    container: {
-        padding: { xs: '16px', md: '35px', display: 'flex' }
-    },
-    productContainer: {
-        width: '80%',
-        padding: '24px',
-        display: 'flex',
-        flexDirection: 'column',
-        alignItems: 'center',
-        gap: '16px',
-    },
-    productImage: {
-        width: '100%',
-        maxWidth: '400px',
-    },
-    productName: {
-        fontSize: '45px',
-        fontWeight: 500,
-    },
-    productGender: {
-        textAlign: 'left',
-        fontSize: '18px',
-        fontWeight: 400,
-    },
-    productPrice: {
-        position: 'absolute',
-        right: '0',
-        bottom: '0',
-        fontSize: '18px',
-        fontWeight: 500,
-    },
-    addToCartButton: {
-        minWidth: '48%',
-        textTransform: 'none',
-        padding: '16px 70px',
-        backgroundColor: '#007bff',
-        color: 'white',
-        borderRadius: '4px',
-        cursor: 'pointer',
-        transition: 'background-color 0.3s',
-        '&:hover': {
-            backgroundColor: '#0056b3',
-        },
-    },
-    textContainer: {
-        position: 'relative',
-        width: '100%',
-        justifyContent: 'space-between',
-        display: 'flex'
-    },
-    btnContainer: {
-        width: '100%',
-        display: 'flex',
-        justifyContent: 'space-between'
-    },
-    picturesContainer: {
-        width:'100%',
-        display: 'flex',
-        justifyContent:'center',
-        alignContent:'center',
-        flexDirection: 'column'
-    },
-    arrayOfImages: {
-        gap:'10px',
-        display: 'flex',
-        justifyContent:'center',
-    }
+  container: {
+    padding: { xs: '16px', md: '35px', display: 'flex', gap: '50px' },
+  },
+  productContainer: {
+    width: '100%',
+    padding: '24px',
+    display: 'flex',
+    flexDirection: 'column',
+    alignItems: 'center',
+    gap: '20px',
+  },
+  productImage: {
+    width: '100%',
+    maxWidth: '400px',
+  },
+  productName: {
+    fontSize: '45px',
+    fontWeight: 500,
+  },
+  productLabel: {
+    textAlign: 'left',
+    fontSize: '18px',
+    fontWeight: 400,
+  },
+  productPrice: {
+    position: 'absolute',
+    right: '0',
+    bottom: '0',
+    fontSize: '18px',
+    fontWeight: 500,
+  },
+  addToCartButton: {
+    minWidth: '48%',
+    padding: '16px 70px',
+    borderRadius: '10px',
+    cursor: 'pointer',
+    transition: 'background-color 0.3s',
+  },
+  textContainer: {
+    position: 'relative',
+    width: '100%',
+    justifyContent: 'space-between',
+    display: 'flex',
+  },
+  picturesContainer: {
+    marginBottom: '20px',
+    width: '100%',
+  },
+  buttonSize: {
+    backgroundColor:'blue',
+    padding: '12px 16px',
+    minWidth: '85px',
+    height: '55px',
+    textTransform: 'none',
+  },
+  sizesContainer: {
+    marginTop: '10px',
+    display: 'flex',
+    flexWrap: 'wrap',
+    gap: '26px',
+    width: '100%',
+  },
+  btnContainer: {
+    marginTop: '10px',
+    display: 'flex',
+    gap: '26px',
+    width: '100%',
+  },
+
+
+  description:{
+    marginTop:'40px',
+    display:'flex',
+    flexDirection:'column',
+    gap:'30px'
+  }
 };
 
+
 const SingleProductPage = () => {
-    const product = products[0];
+  const product = products[0];
 
-    return (
+  return (
+    <Container maxWidth="xl" sx={styles.container}>
+      <Box sx={styles.productContainer}>
+        <ImageSlider />
+      </Box>
+
+      <Box sx={styles.productContainer}>
+        <Box sx={styles.textContainer}>
+          <Typography variant="h1" sx={styles.productName}>
+            {product.name}
+          </Typography>
+          <Typography variant="h4" sx={styles.productPrice}>
+            ${product.price}
+          </Typography>
+        </Box>
+
+        <Box sx={styles.picturesContainer}>
+          <Typography variant="h4" sx={styles.productGender}>
+            {product.gender}'s Shoes
+          </Typography>
+          <Grid container spacing={2}>
+            {products.map((p) => (
+              <Grid item key={p.id} xs={4} sm={3} md={2}>
+                <Card sx={styles.productImageCard}>
+                  <CardMedia component="img" alt={p.name} image={p.image} sx={{ height: '4%' }} />
+                </Card>
+              </Grid>
+            ))}
+          </Grid>
+        </Box>
+
+        <Box sx={styles.sizesContainer}>
+          <Typography variant="h4" sx={styles.productLabel}>
+            Select Size
+          </Typography>
+
+          <Box sx={styles.sizesContainer}>
+            {sizes.map((size, index) => (
+              <Button
+                key={index}
+                variant='outlined'
+                sx={styles.buttonSize}
+              >
+                {size}
+              </Button>
+            ))}
+          </Box>
+        </Box>
 
 
-        <Container maxWidth="xl" sx={styles.container}>
-            {/* <Header/> */}
-            <Box sx={styles.productContainer}>
-                <ImageSlider />
-            </Box>
-            <Box sx={styles.productContainer}>
-                <Box sx={styles.textContainer}>
-                    <Typography variant="h1" sx={styles.productName}>
-                        {product.name}
-                    </Typography>
-                    <Typography variant="h4" sx={styles.productPrice}>
-                        ${product.price}
-                    </Typography>
-                </Box>
+        <Box sx={styles.btnContainer}>
+          <Button isTransparent sx={styles.addToCartButton}>Favorite</Button>
+          <Button isTransparent sx={styles.addToCartButton}>Add to Bag</Button>
+        </Box>
 
 
-                <Box sx={styles.picturesContainer}>
-                    <Typography variant="h4" sx={styles.productGender}>
-                        {product.gender}'s Shoes
-                    </Typography>
-                    <Box   sx={styles.arrayOfImages}>
-                        {products.map((p) => (
-                            <Card key={p.id} sx={styles.productImageCard}>
-                                <CardMedia
-                                    component="img"
-                                    alt={p.name}
-                                    height="80"
-                                    image={p.image}
-                                />
-                            </Card>
-                        ))}
-                    </Box>
-
-                </Box>
-                <Box sx={styles.btnContainer}>
-                    <Button sx={styles.addToCartButton}>Favorite</Button>
-                    <Button sx={styles.addToCartButton}>Add to Bag</Button>
-                </Box>
-
-            </Box>
-        </Container>
-    );
+        <Box sx={styles.description}>
+          <Typography variant="h4">
+            Description
+          </Typography>
+          <Typography variant="h4" sx={styles.productLabel}>
+            Boasting the first-ever Max Air unit created specifically for Nike Sportswear, the Nike Air Max 270 delivers an Air unit that absorbs and gives back energy with every springy step. Updated for modern comfort, it nods to the original, 1991 Air Max 180 with its exaggerated tongue top and heritage tongue logo.
+          </Typography>
+        </Box>
+      </Box>
+    </Container>
+  );
 };
 
 export default SingleProductPage;
-
