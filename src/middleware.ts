887fd82c12fec512e91d414ce61d597df9462892
Yura <jormungand1616@gmail.com>
2023-11-08 00:06:35 +0300

    create middleware for protected routes

---
+++ src/middleware.ts
@@ -0,0 +1,20 @@
+import {getToken} from 'next-auth/jwt';
+import {NextRequest, NextResponse} from 'next/server';
+
+export async function middleware(req: NextRequest) {
+  const pathname = req.nextUrl.pathname;
+  //List of protected routes
+  const protectedPaths = ['/my-products'];
+  const isPathProtected = protectedPaths?.some(path => pathname == path);
+  const res = NextResponse.next();
+  if (isPathProtected) {
+    //it is session info
+    const token = await getToken({req});
+    if (!token) {
+      const url = new URL(`/`, req.url);
+      //url.searchParams.set("callbackUrl", pathname);
+      return NextResponse.redirect(url);
+    }
+  }
+  return res;
+}
