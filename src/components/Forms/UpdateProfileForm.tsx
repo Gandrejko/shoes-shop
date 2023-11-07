de5f6633448745081c052bb9627709488b7f17cc
Nastya Kladova <nstsologub@gmail.com>
2023-11-07 15:22:54 +0100

    + add update profile form functionality

--- src/components/Forms/UpdateProfileForm.tsx
+++ src/components/Forms/UpdateProfileForm.tsx
@@ -4,40 +4,73 @@ import {SubmitHandler, useForm} from 'react-hook-form';
 import {Button} from '../Button/Button';
 import {useMutation} from 'react-query';
 import axios from 'axios';
+import {toast} from 'react-toastify';
+
+type CurUserDataType = {
+  id: number;
+  username: string;
+  firstName: string;
+  lastName: string;
+  email: string;
+  phone: number;
+  password: number;
+};
+
+const updateFormPlaceholders = {
+  phone: 11111,
+  lastName: 'Last Name',
+};
 
 type UpdateProfileFormType = {
-  userName: string;
-  userSurname: string;
+  // id: number;
+  firstName: string;
+  lastName: string;
   email: string;
-  phone: string;
+  phoneNumber: number;
 };
 
-const CURUSERDATA = {
-  id: '123',
-  userName: 'Jane',
-  userSurname: 'Meldrum',
+const CURUSERDATA: Partial<CurUserDataType> = {
+  id: 123,
+  username: 'Jane',
+  firstName: 'Jane',
+  lastName: 'Meldrum',
   email: 'rhc23@mail.com',
-  phone: '(949) 354-2574',
+  phone: 9999,
+  password: 777,
 };
 
-const UpdateProfileForm = userdata => {
+const UpdateProfileForm = () => {
   const {
     register,
     handleSubmit,
     formState: {errors},
-  } = useForm<UpdateProfileFormType>();
+  } = useForm<UpdateProfileFormType>({
+    defaultValues: {
+      firstName: CURUSERDATA.firstName,
+      lastName: CURUSERDATA.lastName || updateFormPlaceholders.lastName,
+      email: CURUSERDATA.email,
+      phoneNumber: CURUSERDATA.phone || updateFormPlaceholders.phone,
+    },
+  });
 
-  const {mutateAsync, isSuccess} = useMutation({
-    mutationKey: '',
+  const {mutateAsync} = useMutation({
     mutationFn: (userUpdateData: Partial<UpdateProfileFormType>) =>
       axios.put(
         `https://shoes-shop-strapi.herokuapp.com/api/users${CURUSERDATA.id}`,
         userUpdateData,
       ),
+    onSuccess: () => {
+      toast.success('You profile data is successfully updated!');
+    },
+    onError: () => {
+      toast.error('Something went wrong. Please, try again.');
+    },
   });
 
-  const formSubmitHadler: SubmitHandler<UpdateProfileFormType> = async data => {
-    mutateAsync(data);
+  const formSubmitHadler: SubmitHandler<
+    UpdateProfileFormType
+  > = async updatedData => {
+    mutateAsync(updatedData);
   };
 
   return (
@@ -48,62 +81,65 @@ const UpdateProfileForm = userdata => {
     >
       <Box sx={{mb: 7}}>
         <Input
+          defaultValue={CURUSERDATA.firstName}
           type="text"
-          defaultValue={CURUSERDATA.userName}
           labelText="Name"
-          name="userName"
+          name="firstName"
           register={register}
           validationSchema={{
-            pattern: {
-              required: true,
-            },
+            required: 'Name is required',
           }}
+          required={true}
           style={{marginBottom: 24}}
         />
         <Input
+          defaultValue={CURUSERDATA.lastName}
           type="text"
-          defaultValue={CURUSERDATA.userSurname}
           labelText="Surname"
-          name="userSurname"
+          name="lastName"
           register={register}
           validationSchema={{
-            required: true,
+            required: 'Surname is required',
           }}
+          required={true}
           style={{marginBottom: 24}}
         />
         <Input
-          type="mail"
           defaultValue={CURUSERDATA.email}
+          type="mail"
           labelText="Email"
           name="email"
           register={register}
           disabled
           validationSchema={{
+            required: 'Email is required',
             pattern: {
               value: /\S+@\S+\.\S+/,
+              message: 'This email is invalid',
             },
           }}
+          required={true}
           style={{marginBottom: 24}}
         />
         <Input
-          type="tel"
           defaultValue={CURUSERDATA.phone}
+          type="tel"
           labelText="Phone number"
           name="phoneNumber"
           register={register}
           validationSchema={{
-            required: true,
+            required: 'Phone number is required',
             pattern: {
               value: /^(\d{3})\s\d{3}-\d{4}/,
             },
           }}
+          required={true}
         />
       </Box>
-      <Button width="152" alignSelf="flex-end">
+      <Button width="152" alignSelf="flex-end" type="submit">
         Save changes
       </Button>
     </Box>
-    // {isSuccess && //To show success message}
   );
 };
 
