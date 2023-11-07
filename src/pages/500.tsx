004224893b5b8a6e34e34a85da0c2e8357a9d21f
Antonina Vechorko <antonina.vechorko@gmail.com>
2023-11-07 23:09:28 +0300

    * fix 500page

--- src/pages/500.tsx
+++ src/pages/500.tsx
@@ -1,30 +1,51 @@
 import Image from 'next/image';
-import {Box, CssBaseline, Typography, Stack} from '@mui/material';
-import {CustomButton} from '@/components/Button/Button';
+import {Box, Typography, Stack, SxProps} from '@mui/material';
+import {Button} from '@/components/Button/Button';
+import Header from '@/components/Header';
+
+const styles: Record<string, SxProps> = {
+  page: {
+    height: "100vh",
+  },
+  main: {
+    width: "100vw",
+    height: {sm: "calc(100vh - 60px)", md: "calc(100vh - 120px)"},
+    position: 'relative',
+    paddingTop: "10%",
+    paddingLeft: "10%",
+  },
+  wrapper: {
+    display: "flex",
+    flexDirection: "column",
+    gap: "28px",
+    width: "538px",
+  },
+  text: {
+    zIndex: 1,
+  }
+}
 
 const Error500 = () => {
   return (
-    <>
-      <CssBaseline />
-      <Box sx={{minHeight: "100vh"}}>
-          <header style={{ height: "120px"}}></header>
-          <Box sx={{width: "100vw", height: "calc(100vh - 120px)", position: 'relative', paddingTop: "10%", paddingLeft: "10%"}}>
+      <Box sx={styles.page}>
+          <Header />
+          <Box sx={styles.main}>
             <Image
               src="/images/500PageBanner.png"
               alt="500 error"
-              fill={true}
+              fill
+              objectFit="cover"
             />
-            <Box sx={{ display: "flex", flexDirection: "column", gap: "20px", width: "538px"}}>
-              <Typography component="h1" sx={{fontSize: "45px", fontWeight: "bold", zIndex: 1}}>We lost that page...</Typography>
-              <Typography component="span" sx={{color: "#5C5C5C", fontSize: "20px", lineHeight: "28px", zIndex: 1}}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna</Typography>
+            <Box sx={styles.wrapper}>
+              <Typography variant="h1" sx={styles.text}>We lost that page...</Typography>
+              <Typography variant="h4" sx={styles.text}>Lorem ipsum dolor sit amet, consectetuer adipiscing elit, sed diam nonummy nibh euismod tincidunt ut laoreet dolore magna</Typography>
               <Stack direction="row" spacing={4}>
-                <CustomButton width="152px" height="40px" isTransparent={true}>Go Back</CustomButton>
-                <CustomButton width="152px" height="40px" isTransparent={false}>Home</CustomButton>
+                <Button width="152px" height="40px" isTransparent={true}>Go Back</Button>
+                <Button width="152px" height="40px" isTransparent={false}>Home</Button>
               </Stack>
             </Box>
           </Box>
       </Box>
-    </>
   );
 };
 
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,5 @@
 import React from 'react';
 import Header from '@/components/Header';
-import Link from 'next/link'
 
 export default function Home() {
   // const {data: session} = useSession();
@@ -8,9 +7,7 @@ export default function Home() {
   return (
     <>
       {/* <CustomButton width="90%" isTransparent={false}>submit</CustomButton> */}
-      {/*<Header />*/}
-      <Link href="/404">404</Link>
-      <Link href="/500">500</Link>
+      <Header />
     </>
   )
 }
