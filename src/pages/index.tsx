df3f109bfbfe633574673ed41efaeb1af58e28cd
ste1nerr <oachkevych@gmail.com>
2023-11-07 15:19:02 +0200

    * resolve conflicts

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,3 @@
-import React from 'react';
-import {CustomButton} from '../components/Button/Button';
-import ButtonMenu from '../components/Product/ButtonMenu';
-import EmptyCartPage from '@/components/Cart/EmptyCartPage';
-
 export default function Home() {
   return <></>;
 }
