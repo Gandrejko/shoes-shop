165af26e78985ad56dcdaf2d1e173d12ae3bca9e
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-11-08 03:35:43 +0100

    + add all http methods

--- src/services/api-client.ts
+++ src/services/api-client.ts
@@ -4,25 +4,30 @@ const axiosInstance = axios.create({
   baseURL: 'https://shoes-shop-strapi.herokuapp.com/api',
 });
 
-class APIClient<T> {
+class APIClient<Req = any, Res = any> {
   private _endpoint: string;
 
   constructor(endpoint: string) {
     this._endpoint = endpoint;
   }
 
-  getSingle = async (id: string | number) => {
-    const res = await axiosInstance.get<T>(`${this._endpoint}/${id}`);
+  get = async (config?: AxiosRequestConfig) => {
+    const res = await axiosInstance.get<Res>(this._endpoint, config);
     return res.data;
   };
 
-  getAll = async (config: AxiosRequestConfig) => {
-    const res = await axiosInstance.get<T>(this._endpoint, config);
+  post = async (data: Req, config?: AxiosRequestConfig) => {
+    const res = await axiosInstance.post<Res>(this._endpoint, data, config);
     return res.data;
   };
 
-  post = async (data: T) => {
-    const res = await axiosInstance.post<T>(this._endpoint, data);
+  put = async (data: Req, config?: AxiosRequestConfig) => {
+    const res = await axiosInstance.put<Res>(this._endpoint, data, config);
+    return res.data;
+  };
+
+  delete = async (config: AxiosRequestConfig) => {
+    const res = await axiosInstance.delete<Res>(this._endpoint, config);
     return res.data;
   };
 }
