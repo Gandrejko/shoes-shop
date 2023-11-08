d052b5c8edfdef7fc4992c9647bd0cbaa4de5af6
Yura <jormungand1616@gmail.com>
2023-11-08 15:02:03 +0300

    set accessToken to localStorage if the user authenticated

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,20 @@
+import {useSession} from 'next-auth/react';
+import {useEffect} from 'react';
+
 export default function Home() {
+  const {data: session, status} = useSession();
+
+  useEffect(() => {
+    if (status === 'authenticated') {
+      const value = localStorage.getItem('accessToken');
+
+      if (value === null || value === undefined) {
+        localStorage.setItem(
+          'accessToken',
+          JSON.stringify(session?.user.accessToken),
+        );
+      }
+    }
+  });
   return <></>;
 }
