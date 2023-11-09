89f2d7abc7bf08fa6186b92ea6a5f32291f40535
ste1nerr <oachkevych@gmail.com>
2023-11-09 16:27:39 +0200

    remove custom button

--- src/components/Cart/EmptyCartPage.tsx
+++ src/components/Cart/EmptyCartPage.tsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import {Box, Typography} from '@mui/material';
+import {Box, Typography, Button} from '@mui/material';
 import {CustomButton} from '@/components/Button/Button';
 import Image from 'next/image';
 
