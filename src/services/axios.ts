a6d8f1354e59af899b0ad8971436fd8175511db8
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-11-10 01:53:07 +0100

    * add token to every request header

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -2,9 +2,15 @@ import axiosInstance from 'axios';
 
 const axios = axiosInstance.create({
   baseURL: 'https://shoes-shop-strapi.herokuapp.com/api',
-  headers: {
-    Authorization: `Bearer eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mzk1LCJpYXQiOjE2OTkzNzU4OTQsImV4cCI6MTcwMTk2Nzg5NH0.Toa8YhgAK-KC1FWVmbwLLTUrRpsZHdOZ7_fvTl_Mei0`,
-  },
+});
+
+axios.interceptors.request.use(config => {
+  const token = localStorage.getItem('token');
+  if (token) {
+    config.headers.Authorization = token ? `Bearer ${token}` : '';
+  }
+
+  return config;
 });
 
 export default axios;
