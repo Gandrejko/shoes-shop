0003930d52dcac24cbebd79198daa6c31f606afc
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-11-13 03:05:22 +0100

    + add temporary token until getting the correct one

--- src/services/axios.ts
+++ src/services/axios.ts
@@ -5,7 +5,8 @@ const axios = axiosInstance.create({
 });
 
 axios.interceptors.request.use(config => {
-  const token = localStorage.getItem('token');
+  const token =
+    'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6Mzk1LCJpYXQiOjE2OTkzNzU4OTQsImV4cCI6MTcwMTk2Nzg5NH0.Toa8YhgAK-KC1FWVmbwLLTUrRpsZHdOZ7_fvTl_Mei0';
   if (token) {
     config.headers.Authorization = token ? `Bearer ${token}` : '';
   }
