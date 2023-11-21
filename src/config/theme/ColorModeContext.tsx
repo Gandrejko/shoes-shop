0878b4e46a4ce974e647c3b3f3668d5584f64021
Usmonbek Rustamov <u.umarbekovich@gmail.com>
2023-11-21 03:59:23 +0100

    + create ColorMode Context

---
+++ src/config/theme/ColorModeContext.tsx
@@ -0,0 +1,28 @@
+import {getPalette} from '@/config/theme/getThemeProps';
+import {Theme, createTheme} from '@mui/material';
+import {createContext, useMemo, useState} from 'react';
+import commonTheme from '.';
+
+type ContextProps = {
+  toggleTheme: () => void;
+  theme: Theme;
+};
+
+const ColorModeContext = createContext({} as ContextProps);
+
+const useColorMode = () => {
+  const [mode, setMode] = useState<'light' | 'dark'>('light');
+
+  const theme = useMemo(() => {
+    return createTheme(commonTheme, getPalette(mode));
+  }, [mode]);
+
+  const toggleTheme = () => {
+    setMode(prevMode => (prevMode === 'light' ? 'dark' : 'light'));
+  };
+
+  return [theme, toggleTheme] as [typeof theme, typeof toggleTheme];
+};
+
+export {useColorMode};
+export default ColorModeContext;
