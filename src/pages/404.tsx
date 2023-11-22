15c8c6f5c206a5d14b1859768d3db8108216dbd9
Nastya Kladova <nstsologub@gmail.com>
2023-11-22 11:28:22 +0100

    + add head to error pages

--- src/pages/404.tsx
+++ src/pages/404.tsx
@@ -8,6 +8,7 @@ import {
   Typography,
   useMediaQuery,
 } from '@mui/material';
+import Head from 'next/head';
 import Image from 'next/image';
 import Link from 'next/link';
 import {useRouter} from 'next/router';
@@ -67,6 +68,9 @@ const Error404 = () => {
     <Stack sx={styles.page}>
       {isMobile ? (
         <>
+          <Head>
+            <title>Not found</title>
+          </Head>
           <Box sx={styles.mobile}>
             <Image
               src="/images/404PageBanner.png"
@@ -107,6 +111,9 @@ const Error404 = () => {
         </>
       ) : (
         <>
+          <Head>
+            <title>Not found</title>
+          </Head>
           <HeaderLayout>
             <Box sx={styles.main}>
               <Box sx={styles.info}>
